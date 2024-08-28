556663d5d6bc6523c00a4107ce67dd3518a128ab
Manish Pandey <manishpandeycoding@gmail.com>
2024-08-28 12:49:34 +0530

    facing error with downloaded pdf file that it is not a valid pdf file.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const puppeteer = require('puppeteer');
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -11,8 +12,40 @@ app.get('/',function(req,res){
     res.status(200).render('index');
 })
 
-app.post('/Pdf',function(req,res){
-    
+app.post('/Pdf',async function(req,res){
+    var {chatLink} = req.body;
+    console.log(chatLink);
+    if(!chatLink)
+    {
+        res.status(404).send('Link is Empty');
+    }
+    else
+    {
+        try
+        {
+            //Ek headless browser launch hoga 
+            const newBrowser = await puppeteer.launch();
+            console.log('headless browser launched');
+            //ek new tab open hoga us browser instance me
+            const newTab = await newBrowser.newPage();
+            console.log('new Tab opened');
+            //chatLink Url me new tab open hoga
+            await newTab.goto(chatLink, {waitUntil : 'networkidle2', timeout : 60000});
+            console.log('Went to the chatLink');
+            
+            const pdfBuffer = await newTab.pdf();
+            console.log('created the pdf buffer');
+
+            res.setHeader('Content-Type', 'application/pdf');
+            console.log('response is being sent from the server');
+
+            res.send(pdfBuffer);
+        } catch (err)
+        {
+            console.log('Something Went Wrong',err);
+        }
+
+    }
 })
 
 app.get('*',function(req,res){
